68b4fb71c8bb050b0e9306b96cef915dab404a5c
rashfael <rashfael@isobeef.org>
2016-12-22 15:14:57 +0100

    update idm endpoints for groups

--- src/idm.js
+++ src/idm.js
@@ -1,9 +1,15 @@
 /* eslint camelcase: "off" */
 const IDM = function (fetch, baseUrl, token) {
 	const api = {
-		chargebee: {
-			portal () {
-				return api.fetch(`chargebee/portal/`, 'POST')
+		groups: {
+			list () {
+				return api.fetch(`groups/`)
+			},
+			get (group) {
+				return api.fetch(`groups/${group}/`)
+			},
+			chargeBeePortal (group) {
+				return api.fetch(`groups/${group}/portal/`, 'POST')
 			}
 		}
 	}
