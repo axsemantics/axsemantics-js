b2a070432f0d0ba5a3e62d929c573d19bb9fa5c9
Andre Etienne <andreetienne1987@gmail.com>
2017-04-13 13:22:13 +0200

    [lexicon] Add options{} for more flexibility.

--- src/lexicon.js
+++ src/lexicon.js
@@ -1,9 +1,18 @@
+import querystring from 'querystring'
+import { cleanQuery } from './utils'
 
 const lexicon = function (fetch, baseUrl, token) {
 	const api = {
 		entries: {
-			list (trainings, page, language) {
-				return api.fetch(`lexica/?trainings=${trainings}&page=${page}&language=${language}`)
+			list (trainings, options = {}) {
+				const query = {
+					trainings,
+					page: options.page,
+					language: options.language,
+					o: options.modified
+				}
+				const qs = querystring.stringify(cleanQuery(query))
+				return api.fetch(`lexica/?${qs}`)
 			},
 			create (lexiconEntry) {
 				return api.fetch('lexica/', 'POST', lexiconEntry)
