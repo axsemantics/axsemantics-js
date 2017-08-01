bc2501b18f1fd85d7c4dbd610658c5b6ad32035b
Andre Etienne <andreetienne1987@gmail.com>
2017-08-01 11:20:09 +0200

    [lexicon] delete lexicon entries endpoint

--- src/lexicon.js
+++ src/lexicon.js
@@ -3,36 +3,6 @@ import { cleanQuery } from './utils'
 
 const lexicon = function (fetch, baseUrl, token) {
 	const api = {
-		entries: {
-			list (trainings, options = {}) {
-				const query = {
-					trainings,
-					page: options.page,
-					language: options.language,
-					o: options.modified
-				}
-				const qs = querystring.stringify(cleanQuery(query))
-				return api.fetch(`lexica/?${qs}`)
-			},
-			create (lexiconEntry) {
-				return api.fetch('lexica/', 'POST', lexiconEntry)
-			},
-			delete (id) {
-				return api.fetch(`lexica/${id}/`, 'DELETE')
-			},
-			addTraining (id, trainingId) {
-				return api.fetch(`lexica/${id}/add-training/`, 'POST', {training: trainingId})
-			},
-			removeTraining (id, trainingId) {
-				return api.fetch(`lexica/${id}/remove-training/`, 'POST', {training: trainingId})
-			},
-			getLexiconEntry (id) {
-				return api.fetch(`lexica/${id}/`)
-			},
-			patch (lexiconEntry) {
-				return api.fetch(`lexica/${lexiconEntry.id}/`, 'PATCH', lexiconEntry)
-			}
-		},
 		nouns: {
 			list (trainings, options = {}) {
 				const query = {
