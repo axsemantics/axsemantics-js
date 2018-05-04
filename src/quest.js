ca83c5f83a19e70f1f69b88a66daf79e6a90169a
Tobias Kunze <rixx@cutebit.de>
2018-05-04 10:51:25 +0200

    Quest API is now versioned

--- src/quest.js
+++ src/quest.js
@@ -4,30 +4,30 @@ const Quest = function (fetch) {
 	const api = {
 		quests: {
 			list () {
-				return api.fetch(`quests/`)
+				return api.fetch(`v1/quests/`)
 			},
 			get (id) {
-				return api.fetch(`quests/${id}/`)
+				return api.fetch(`v1/quests/${id}/`)
 			},
 			finish (id, email) {
-				return api.fetch(`quests/${id}/finish/`, 'POST', {email})
+				return api.fetch(`v1/quests/${id}/finish/`, 'POST', {email})
 			}
 		},
 		tasks: {
 			list () {
-				return api.fetch(`tasks/`)
+				return api.fetch(`v1/tasks/`)
 			},
 			get (id) {
-				return api.fetch(`tasks/${id}/`)
+				return api.fetch(`v1/tasks/${id}/`)
 			},
 			finish (id, email) {
-				return api.fetch(`tasks/${id}/finish/`, 'POST', {email})
+				return api.fetch(`v1/tasks/${id}/finish/`, 'POST', {email})
 			}
 		},
 		users: {
 			get (email) {
 				const qs = new URLSearchParams({email}).toString()
-				return api.fetch(`languages/?${qs}`)
+				return api.fetch(`v1/users/?${qs}`)
 			},
 		}
 	}
