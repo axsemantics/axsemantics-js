413266dd7430944fc771f6ebebc6ba4292c08b13
Michael Käufl <aexea@c.michael-kaeufl.de>
2018-10-04 11:59:46 +0200

    [myax] Remove endpoints for contentProjects

--- src/myax.js
+++ src/myax.js
@@ -151,11 +151,6 @@ const MyAx = function (fetch, baseUrl, token) {
 			get (id) {
 				return api.fetch(`v2/story-exports/${id}/`)
 			},
-		},
-		contentProjects: {
-			list (training_id) {
-				return api.fetch(`v1/content-project/?training_id=${training_id}`)
-			},
 		}
 	}
 
