be47f1645ff02a155c15393b6e74cc37ec2ca3e5
rashfael <rashfael@isobeef.org>
2020-01-15 10:30:30 +0100

    Add new output format for browserify
    - new output format: commonjs with browser apis

--- rollup.config.browser.js
+++ rollup.config.browser.js
@@ -4,9 +4,8 @@ import builtins from 'rollup-plugin-node-builtins'
 export default {
 	input: 'src/index.js',
 	output: {
-		name: 'AxSemanticsClient',
-		format: 'iife',
-		file: 'dist/axsemantics.iife.js'
+		format: 'cjs',
+		file: 'dist/axsemantics.browser.js'
 	},
 	plugins: [babel(), builtins()]
 }
